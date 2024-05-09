1dc627e9e09d87e372e982e89a9f3fe42c00531e
Jkwange <90503219+Jkwange@users.noreply.github.com>
2024-05-10 03:04:09 +0900

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("영남대학교<br>3학년은 오늘도 운다")
+    return HttpResponse("지금 시각 03:23<br>컴퓨터공학과 3학년은 오늘도 운다")
